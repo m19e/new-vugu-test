134b5de38d537c292733b4bf2eeb061efdcb3a96
m19e <nexus.me7290@gmail.com>
2020-04-24 22:56:42 +0900

    dist確認用サーバー(アドレス表示)

--- server.go
+++ server.go
@@ -10,6 +10,8 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("dist")))
 
+	log.Printf("Starting HTTP Server at %q", "127.0.0.1:8686")
+
 	if err := http.ListenAndServe(":8686", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
